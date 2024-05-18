5de893ae83b3faea04120d5b9c4a3355717b0f1d
Kristina Ackovska <ackovska.kristina@gmail.com>
2024-05-18 18:40:47 +0200

    exercise-2

--- index-workshop.js
+++ index-workshop.js
@@ -1,1 +1,12 @@
-// Test
+let day = "Sunday";
+
+if (day === "Monday") {
+    console.log("Start of the work week!");
+} else if (day === "Wednesday") {
+    console.log("Midweek!");
+} else if (day === "Friday") {
+    console.log("Almost weekend!");
+} else {
+    console.log("Just another day");
+}
+
