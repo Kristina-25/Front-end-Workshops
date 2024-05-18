25f9c45f0a34cf382bba391f39d3fa642b5e2ea6
Kristina Ackovska <ackovska.kristina@gmail.com>
2024-05-18 17:05:25 +0200

    First commit

---
+++ index-workshop.js
@@ -0,0 +1,13 @@
+
+
+let age = 25
+
+if (age<=12) {
+    console.log("Child ticket: $5");   
+}  else if (age <=18) {
+    console.log("Teens ticket: $7");
+} else  if (age >18 <=60) {
+    console.log("Adult ticket: $10");
+} else if (age >60) {
+    console.log("Senior ticket");
+}
