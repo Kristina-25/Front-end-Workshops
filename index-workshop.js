6f505c311247407c176fa000b9f7b8dd1f5eadfa
Kristina Ackovska <ackovska.kristina@gmail.com>
2024-05-18 19:18:43 +0200

    finished ex-4

---
+++ index-workshop.js
@@ -0,0 +1,10 @@
+let username = "admin";
+let password = " Welcome";
+
+if (username === "admin" && password === " Welcome") {
+    console.log("admin!");
+} else if (username === "user123 " && password === "password123") {
+    console.log ("user123!"); 
+} else {
+    console.log("Invalid username or password");
+}
