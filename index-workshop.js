2001b8763489c092b20d09bba6876be9a79738db
Kristina Ackovska <ackovska.kristina@gmail.com>
2024-05-18 19:39:36 +0200

    finished ex-5

---
+++ index-workshop.js
@@ -0,0 +1,11 @@
+let age = 22;
+
+if (age >= 18) {
+    console.log("You can vote");
+} else {
+    console.log("You can not vote");
+}
+
+
+let result = age <= 18 ? ("You can't vote") : ("You can vote");
+console.log(result);
