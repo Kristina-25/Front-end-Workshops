83364908343550b64b39c769416cbedd04b87530
Kristina Ackovska <ackovska.kristina@gmail.com>
2024-05-18 18:55:00 +0200

    finished ex-3

---
+++ index-workshop.js
@@ -0,0 +1,10 @@
+
+let weather = "sunny" 
+
+if (weather === "sunny") {
+    console.log("Wear sunglasses!");
+} else if (weather === "rainy") {
+    console.log("Take an umbrella!");
+} else if (weather === "snowy" ) {
+    console.log("Wear a warm coat!");
+} else console.log("Check the weather forecast for more details");
